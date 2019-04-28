ac5e946dbf9c53f147cf8e6782d04c2b3e1694e0
wilsenock220 <wilsenock220@gmail.com>
2019-04-28 14:42:52 +0300

    add new images

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
 
-
-var side1 = parseInt(prompt("Enter the first length:"));{
-var side2 = parseInt(prompt("Enter the second length:"));{
-var side3 = parseInt(prompt("Enter the third length:"));{
-if (side1===side2 && side2===side3) {
-    console.log("This is an equilateral triangle! All the sides are equal.");
-  }
-  else if(side1 !== side2 && side2 !== side3 && side1 !== side3) {
-    console.log("This is a scalene triangle!");
-  };
-   else if (side1 === side2 && side2 !== side3 || side2 === side3 && side3 !== side1 || side1 === side3 && side1 !== side2) {
-    console.log("This is an isosceles triangle!");
-  };
-   else if (temperature < 25) {
-    console.log("Have a nice day :)");
-  }
-   else {
-    console.log("Keep cool today!");
-  }
-}
-}
-}
+// 
+// function result(){
+//
+// var side1=parseInt(document.getElementById('side1').value);
+// var side2=parseInt(document.getElementById('side2').value);
+// var side3=parseInt(document.getElementById('side3').value);
+//
+// var answer=document.getElementById('answer');
+//
+// if(side1+side2>side3 && side2+side3>side1 && side1+side3>side2) {
+//   if (side1 == side2&&side1 == side3) {
+//     console.log("This is an equilateral triangle");
+//   }
+//   else if(side1===side2||side2===side3||side1===side3){
+//     console.log("This is an isosceles triangle");
+//   }
+//   else if(side1 !== side2 && side2 !== side3 && side3 !== side1){
+//     console.log("This is a scalene triangle");
+//   }
+// }
+//  else {
+//   console,log("Not a triangle!");
+// }
