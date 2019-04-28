7b26e201dc6d0bbec610a0927899a63d9608db48
wilsenock220 <wilsenock220@gmail.com>
2019-04-28 22:39:47 +0300

    add the READMe file

---
+++ README.md
@@ -0,0 +1,29 @@
+## Triangle Tracker
+### Author:
+- Wilstan Enock Onditi
+
+### Description:
+This is an app that assists mostly students to detect different triangles and their simple properties that differentiate them. The app is programmed to detect triangles such as
+
+### I managed to use the following languages:
+- HTML
+- CSS
+- Javascript
+
+### BDD:
+#### Here is a simple description of how the app works:
+- It shows the user what the app is all about about, tracking triangles.
+- It also shows the user the different types of triangles. There is also a small form where the user fills in the values of each side of the triangle and when one clicks the confirm button they see the type of triangle whose values they've entered.
+- The app is very accurate being able to recognise the values , evaluate the values and bring out the type of   triangle.
+
+### For one to install this app, you need:
+- a device that has access to the internet.
+- A web browser.
+- A source of internet connection.
+
+### below is a link to the repository that will direct you to the app:
+https://github.com/wilsenock220/Triangle-tracker
+
+### License:
+
+Copyright@2019; All rights reserved.
--- scripts.js
+++ scripts.js
@@ -1,24 +1,25 @@
+function result(){
 
-// 
-// function result(){
-//
-// var side1=parseInt(document.getElementById('side1').value);
-// var side2=parseInt(document.getElementById('side2').value);
-// var side3=parseInt(document.getElementById('side3').value);
-//
-// var answer=document.getElementById('answer');
-//
-// if(side1+side2>side3 && side2+side3>side1 && side1+side3>side2) {
-//   if (side1 == side2&&side1 == side3) {
-//     console.log("This is an equilateral triangle");
-//   }
-//   else if(side1===side2||side2===side3||side1===side3){
-//     console.log("This is an isosceles triangle");
-//   }
-//   else if(side1 !== side2 && side2 !== side3 && side3 !== side1){
-//     console.log("This is a scalene triangle");
-//   }
-// }
-//  else {
-//   console,log("Not a triangle!");
-// }
+var side1=parseInt(document.getElementById('side1').value);
+var side2=parseInt(document.getElementById('side2').value);
+var side3=parseInt(document.getElementById('side3').value);
+
+var answer=document.getElementById('answer');
+
+if(side1+side2 > side3 && side2+side3 > side1 && side1+side3 > side2) {
+  if (side1 == side2 && side1 == side3) {
+    console.log("This is an equilateral triangle!");
+  };
+
+  else if(side1 === side2 || side2 === side3 || side1 === side3){
+    console.log("This is an isosceles triangle!");
+  };
+
+  else if(side1 !== side2 && side2 !== side3 && side3 !== side1){
+    console.log("This is a scalene triangle!");
+  };
+};
+
+ else {
+  console,log("Not a triangle!");
+};
