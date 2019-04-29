a80d8493c546b01ef50da5548d29df265a3587f2
wilsenock220 <wilsenock220@gmail.com>
2019-04-29 08:10:13 +0300

    add new styling

--- scripts.js
+++ scripts.js
@@ -1,25 +1,28 @@
-function result(){
-
 var side1=parseInt(document.getElementById('side1').value);
 var side2=parseInt(document.getElementById('side2').value);
 var side3=parseInt(document.getElementById('side3').value);
 
+function triangle(){
+
 var answer=document.getElementById('answer');
-
 if(side1+side2 > side3 && side2+side3 > side1 && side1+side3 > side2) {
-  if (side1 == side2 && side1 == side3) {
-    console.log("This is an equilateral triangle!");
-  };
-
-  else if(side1 === side2 || side2 === side3 || side1 === side3){
-    console.log("This is an isosceles triangle!");
-  };
-
-  else if(side1 !== side2 && side2 !== side3 && side3 !== side1){
-    console.log("This is a scalene triangle!");
-  };
-};
-
+  if (side1==side2&&side1==side3) {
+    answer.innerHTML='Equilateral: All sides are equal!';
+  }
+  else if(side1===side2||side2===side3||side1===side3){
+    answer.textContent='Isosceles: Two sides are equal!'
+  }
+  else{
+    answer.textContent="Scalene: No sides are equal!";
+  }
+}
  else {
-  console,log("Not a triangle!");
-};
+  answer.textContent="Not a triangle!";
+}
+}
+function reset(){
+  var side1=parseInt(document.getElementById('side1').value);
+  var side2=parseInt(document.getElementById('side2').value);
+  var side3=parseInt(document.getElementById('side3').value);
+  var answer=document.getElementById('answer');
+}
