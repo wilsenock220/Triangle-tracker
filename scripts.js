a27859dff9eac82abe6cbc30b86ae9dfb95fdf95
wilsenock220 <wilsenock220@gmail.com>
2019-05-01 07:42:20 +0300

    add new form and improved javascript content

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
-var side1=parseInt(document.getElementById('side1').value);
-var side2=parseInt(document.getElementById('side2').value);
-var side3=parseInt(document.getElementById('side3').value);
+function result(){
+var length1=parseInt(document.getElementById('length1').value);
+var length2=parseInt(document.getElementById('length2').value);
+var length3=parseInt(document.getElementById('length3').value);
+var answer=document.getElementById('answer');
+if(length1+length2 > length3 && length2+length3 > length1 && length1+length3 > length2) {
 
-function triangle(){
-
-if(side1+side2 <= side3 && side2 + side3 <= side1 && side1 + side3 <= side2) {
-   document.getElementById('answer').innerHTML = "Not a triangle";
- }
-  else if (side1 === side2 && side1 === side3 && side2 === side3) {
-    document.getElementById('answer').innerHTML = "Equilateral";
+  if (length1 === length2 && length1 === length3) {
+    answer.textContent='This is an Equilateral triangle!';
   }
-  else if(side1===side2 || side2===side3 ||side1===side3){
-    document.getElementById('answer').innerHTML = "Isosceles";
+  else if(length1 === length2 || length2 === length3 || length1 === length3){
+    answer.textContent='This is an isosceles triangle'
   }
   else{
-    document.getElementById('answer').innerHTML = "scalene";
+    answer.textContent="This is a scalene triangle";
+    // alert("This is a scalene triangle!")
   }
 }
-
+ else {
+  answer.textContent="This is not a triangle!";
+}
+}
 function reset(){
   var side1=parseInt(document.getElementById('side1').value);
   var side2=parseInt(document.getElementById('side2').value);
   var side3=parseInt(document.getElementById('side3').value);
   var answer=document.getElementById('answer');
-};
+}
